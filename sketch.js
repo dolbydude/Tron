8a62762b9d75de3d6a52a1f25ed487f0858db886
Ryan L Nelson-Flack <rn01094@srcs.loc>
2017-04-17 14:40:00 -0700

    updates

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var blueBox;
 var whiteBox;
 var redBox;
 var greenBox;
+var barriers[];
 
 function setup() {
     frameRate(60);
@@ -14,7 +15,7 @@ function setup() {
     planeSize = displayHeight * (.9);
     cornerValue = planeSize * (.4);
     createCanvas(canvasWidth, canvasHeight, WEBGL);
-    controls options : wasd, ARROWS, ijkl, 8456
+ //   controls options : wasd, ARROWS, ijkl, 8456
     blueBox = new Bike("Blue", 1);
     whiteBox = new Bike("White", 2);
     redBox = new Bike("Red", 3);
@@ -50,6 +51,8 @@ function Bike(c, controls) {
     //CONSTRUCTOR/////////////////////////////////////////////////
     this.c = c;
     this.controls = controls;
+    this.turnCount;
+    this.m = "horizontal";
     if (this.c == "Green") {
         this.c1 = 0;
         this.c2 = 255;
@@ -59,6 +62,9 @@ function Bike(c, controls) {
         this.rads = 0;
         this.xVelocity = 3;
         this.yVelocity = 0;
+        barriers.push new Barrier(this.translationX, this.translationY,this.c1,this.c2,this.c3,this.rads);
+        this.turnCount = 0;
+
     }
     if (this.c == "Red") {
         this.c1 = 255;
@@ -69,6 +75,8 @@ function Bike(c, controls) {
         this.rads = PI;
         this.xVelocity = -3;
         this.yVelocity = 0;
+        barriers.push new Barrier(this.translationX, this.translationY,this.c1,this.c2,this.c3,this.rads);
+        this.turnCount = 0;
     }
     if (this.c == "White") {
         this.c1 = 255;
@@ -79,9 +87,10 @@ function Bike(c, controls) {
         this.rads = PI;
         this.xVelocity = -3;
         this.yVelocity = 0;
+        barriers.push new Barrier(this.translationX, this.translationY,this.c1,this.c2,this.c3,this.rads);
+        this.turnCount = 0;
     }
     if (this.c == "Blue") {
-        push();
         this.c1 = 0;
         this.c2 = 0;
         this.c3 = 255;
@@ -90,7 +99,8 @@ function Bike(c, controls) {
         this.rads = 0;
         this.xVelocity = 3;
         this.yVelocity = 0;
-        pop();
+        barriers.push new Barrier(this.translationX, this.translationY,this.c1,this.c2,this.c3,this.rads);
+        this.turnCount = 0;
     }
     //CONTRUCTOR ///////////////////////////////////////////////////////////
     this.display = function () {
@@ -104,11 +114,15 @@ function Bike(c, controls) {
     this.move = function () {
         this.translationX = this.translationX + this.xVelocity;
         this.translationY = this.translationY + this.yVelocity;
+        barriers[this.turnCount].move(this.xVelocity,this.yVelocity,this.m);
+        barriers[this.turnCount].display();
         if (this.controls == 1) {
             if (keyCode == UP_ARROW) {
                 this.rads = HALF_PI;
                 this.xVelocity = 0;
                 this.yVelocity = -3;
+                this.m = "verticle";
+                this.turnCount = this.turnCount++;
             }
             else if (keyCode == LEFT_ARROW) {
                 this.rads = PI;
@@ -200,12 +214,45 @@ function Bike(c, controls) {
     //  this.lose = function () {};
 }
 
-//function Barrier(x, y, w, h) {
-//    this.x = x;
-//    this.y = y;
-//    this.w = w;
-//    this.h = h;
-//    this.display = function () {};
-//    this.move = function () {};
-//    this.collisions = function () {};
-//};
+function Barrier(x, y,c1,c2,c3,rads) {
+    //initial x and y position
+    this.x = x;
+    this.y = y;
+    //rotation
+    this.rads = rads;
+    //colors
+    this.c1 = c1;
+    this.c2 = c2;
+    this.c3 = c3;
+    //velocity
+    this.vX;
+    this.vY;
+    //length of barrier
+    this.length;
+
+    this.display = function () {
+        push();
+        fill(this.c1, this.c2, this.c3);
+        translate(this.x, this.y,5);
+        rotateZ(this.rads);
+        box(length, 10, 10);
+        pop();
+    };
+    this.move = function (bikeVelocityX,bikeVelocityY, m){
+        this.vX = bikeVelocityX/2;
+        this.vY = bikeVelocityY/2;
+        this.x = this.x+this.vX;
+        this.y = this.y+this.vY;
+        this.movement = m;
+        if(this.movement = "verticle"){
+            this.length = this.length + this.vY*(2);
+        }
+        if(this.movement = "horizontal") {
+        this.length = this.length + this.vX*(2);
+        }
+    };
+    this.collisions = function () {
+
+
+    };
+};
